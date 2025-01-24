0b454e4a483d68bf2d564f8c6197f8d4a579a696
Azra <Azra.Talic@infineon.com>
2025-01-24 08:35:18 +0100

    Multichannel ADC

---
+++ libraries/SensEdu/examples/Read_ADC_3CH_DMA/Read_ADC_3CH_DMA.ino
@@ -0,0 +1,93 @@
+#include "SensEdu.h"
+
+uint32_t lib_error = 0;
+uint32_t cntr = 0;
+
+/* -------------------------------------------------------------------------- */
+/*                                  Settings                                  */
+/* -------------------------------------------------------------------------- */
+ADC_TypeDef* adc = ADC1;
+const uint8_t adc_pin_num = 3;
+uint8_t adc_pins[adc_pin_num] = {A0, A1, A2}; 
+
+// must be:
+// 1. multiple of 32 words (64 half-words) to ensure cache coherence
+// 2. properly aligned
+const uint16_t memory4adc_size = 64 * adc_pin_num; // allocate chunks of (4 * 32) * number of channels
+__attribute__((aligned(__SCB_DCACHE_LINE_SIZE))) uint16_t memory4adc[memory4adc_size];
+
+/* -------------------------------------------------------------------------- */
+/*                                    Setup                                   */
+/* -------------------------------------------------------------------------- */
+void setup() {
+
+    // doesn't boot without opened serial monitor
+    Serial.begin(115200);
+    while (!Serial) {
+        delay(1);
+    }
+    Serial.println("Started Initialization...");
+
+    SensEdu_Init(adc, adc_pins, adc_pin_num, SENSEDU_ADC_MODE_CONT, 1000, SENSEDU_ADC_DMA_CONNECT); // continuos mode for ADC
+    SensEdu_ADC_Enable(adc);
+
+    SensEdu_DMA_Init((uint16_t*)memory4adc, memory4adc_size); 
+    SensEdu_DMA_Enable((uint16_t*)memory4adc, memory4adc_size);
+
+    SensEdu_ADC_Start(adc);
+
+    lib_error = SensEdu_GetError();
+    while (lib_error != 0) {
+        delay(1000);
+        Serial.print("Error: 0x");
+        Serial.println(lib_error, HEX);
+    }
+
+    Serial.println("Setup is successful.");
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                    Loop                                    */
+/* -------------------------------------------------------------------------- */
+void loop() {
+    // CPU does something
+    cntr += 1;
+    Serial.print("Counter value: ");
+    Serial.println(cntr);
+    
+    // DMA in background
+
+    // Print transfered Data if available
+    if (SensEdu_DMA_GetTransferStatus()) {
+        Serial.println("------");
+        for (int i = 0; i < memory4adc_size; i+=3) {
+            Serial.print("ADC value ");
+            Serial.print(i/3);
+            Serial.print(" for channel 0: ");
+            Serial.println(memory4adc[i]);
+
+            Serial.print("ADC value ");
+            Serial.print(i/3);
+            Serial.print(" for channel 1: ");
+            Serial.println(memory4adc[i+1]);
+
+            Serial.print("ADC value ");
+            Serial.print(i/3);
+            Serial.print(" for channel 2: ");
+            Serial.println(memory4adc[i+2]);
+        };
+
+        // restart ADC
+        SensEdu_DMA_ClearTransferStatus();
+        SensEdu_DMA_Enable((uint16_t*)memory4adc, memory4adc_size);
+        SensEdu_ADC_Start(adc);
+    }
+
+    // check errors
+    lib_error = SensEdu_GetError();
+    while (lib_error != 0) {
+        delay(1000);
+        Serial.print("Error: 0x");
+        Serial.println(lib_error, HEX);
+    }
+}

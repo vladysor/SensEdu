399df34d7850fca157db42a876d82edef3cef6b9
Vladyslav Sorokin <pan.vladsorokin@gmail.com>
2025-02-03 03:47:10 +0100

    fixed FOUC - basic layout finished

--- docs/assets/js/header-buttons.js
+++ docs/assets/js/header-buttons.js
@@ -1,75 +1,32 @@
-const gitIcon = document.getElementById("git-logo");
-const themeIcon = document.getElementById("theme-logo");
+const themeSwitcher = document.querySelector('.js-theme-switcher');
+const openGitHub = document.querySelector('.js-git-open');
+
 const gitButton = document.getElementById("git-button");
 const themeButton = document.getElementById("theme-button");
 
-const DARK_THEME = "dark";
-const LIGHT_THEME = "light";
-
-function openGitHub() {
-    window.open('https://github.com/ShiegeChan/SensEdu','_blank');
-    gitButton.blur(); // remove button focus
-}
-
-function changeTheme() {
-    const currentTheme = jtd.getTheme();
-    if (currentTheme === DARK_THEME) {
-        applyTheme(LIGHT_THEME);
+jtd.addEvent(themeSwitcher, 'click', function() {
+    if (jtd.getTheme() === 'dark') {
+        jtd.setTheme('default');
+        gitButton.className = 'header-button hb_light js-git-open';
+        themeButton.className = 'header-button hb_light js-theme-switcher';
     } else {
-        applyTheme(DARK_THEME);
-    }
-}
-
-function applyTheme(new_theme) {
-    jtd.setTheme(new_theme);
-
-    if (new_theme === DARK_THEME) {
-        gitIcon.src = "/assets/images/github-logo-light.png";
-        themeIcon.src = "/assets/images/sun.png";
-
-        gitButton.className = 'header-button hb_dark';
-        themeButton.className = 'header-button hb_dark';
-    } else {
-        gitIcon.src = "/assets/images/github-logo-dark.png";
-        themeIcon.src = "/assets/images/moon.png";
-
-        gitButton.className = 'header-button hb_light';
-        themeButton.className = 'header-button hb_light';
+        jtd.setTheme('dark');
+        gitButton.className = 'header-button hb_dark js-git-open';
+        themeButton.className = 'header-button hb_dark js-theme-switcher';
     }
 
-    themeButton.blur(); // remove button focus
-    saveTheme(new_theme, gitIcon.src, themeIcon.src, gitButton.className, themeButton.className);
-}
-
-function saveTheme(new_theme, git_icon_src, theme_icon_src, git_button_class, theme_button_class) {
-    localStorage.setItem("theme", new_theme);
-    localStorage.setItem("git-icon", git_icon_src);
-    localStorage.setItem("theme-icon", theme_icon_src);
-    localStorage.setItem("git-button-class", git_button_class);
-    localStorage.setItem("theme-button-class", theme_button_class);
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-
-    // load all data from local storage
-    // because these settings are not global for each page
-    // for some magical reason
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme == DARK_THEME || savedTheme == LIGHT_THEME) {
-        jtd.setTheme(savedTheme);
-
-        const savedGitIconSrc = localStorage.getItem("git-icon");
-        const savedThemeIconSrc = localStorage.getItem("theme-icon");
-        const savedGitButtonClass = localStorage.getItem("git-button-class");
-        const savedThemeButtonClass = localStorage.getItem("theme-button-class");
-        
-        gitIcon.src = savedGitIconSrc;
-        themeIcon.src = savedThemeIconSrc;
-        gitButton.className = savedGitButtonClass;
-        themeButton.className = savedThemeButtonClass;
-    }
-
+    // remove button focus
+    themeButton.blur(); 
 });
 
+jtd.addEvent(openGitHub, 'click', function() {
+    window.open('https://github.com/ShiegeChan/SensEdu','_blank');
+    gitButton.blur(); 
+});
 
-
+document.addEventListener("DOMContentLoaded", function () {
+    if (jtd.getTheme() === 'dark') {
+        gitButton.className = 'header-button hb_dark js-git-open';
+        themeButton.className = 'header-button hb_dark js-theme-switcher';
+    }
+});

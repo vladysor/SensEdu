f0678784b5e4de57497286c6ad3158610b5cd641
Azra <Azra.Talic@infineon.com>
2025-02-07 10:41:35 +0100

    Some progress

--- projects/ultrasound_distance_measurement/matlab/read_mcu_xcorr.m
+++ projects/ultrasound_distance_measurement/matlab/read_mcu_xcorr.m
@@ -3,6 +3,5 @@ function dist_vector = read_mcu_xcorr(arduino, mic_num)
     for i = 1:mic_num
         serial_rx_data = read(arduino, 4, 'uint8'); % 32bit per one distance measurement
         dist_vector(i) = double(typecast(uint8(serial_rx_data), 'uint32'))/1e6; % expected in micrometers
-        %dist_vector(i) = double(typecast(uint8(serial_rx_data), 'uint32'));
     end
 end

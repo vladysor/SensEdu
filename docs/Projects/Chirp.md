bbc478f0a12816967c96dfd80ec5ac9252a83971
PaulThbt <PaulCharles.Thiebaut@infineon.com>
2025-02-26 10:58:24 +0100

    New FMCW project folder, Chirp doc

--- docs/Projects/Chirp.md
+++ docs/Projects/Chirp.md
@@ -25,22 +25,24 @@ Chirp signals are encountered in numerous fields like radar & sonar systems, tel
 ## Chirp Generation Function
 Arduino does not provide any built-in chirp signal function. There are workarounds using MATLAB's built-in chirp function but our idea was to create this signal directly in Arduino with the SensEdu library.
 
-The `generateSawtoothChirp` and `generateTriangularChirp` functions both calculate the values to generate the lookup table (LUT) of a sawtooth chirp and triangular chirp signal respectively.
+The `generateSawtoothChirp` and `generateTriangularChirp` functions both calculate the values to generate the lookup table (LUT) of a sawtooth chirp and triangular chirp respectively.
 
 ```c
 void generateSawtoothChirp(uint16_t* array)
+void generateTriangularChirp(uint16_t* array)
 ```
 
 Parameter 
 * `uint16_t* array` : A pointer to an array where the generated chirp signal will be stored.
 
-
-
-
 ---
 
 ## Main code
-The Chirp_SawtoothMod.ino and Chirp_TriangularMod.ino and
+The `Chirp_SawtoothMod.ino` and `Chirp_TriangularMod.ino` files contain the main code to generate the chirp signal, sending the chirp LUT to the DAC and enabling the DAC. The code contains the following user settings to adjust the chirp signal :
+
+* `CHIRP_DURATION`{: .text-blue-000} : The period of the chirp signal in seconds
+* `START_FREQUENCY`{: .text-blue-000} : The start frequency of the chirp signal in Hz
+* `END_FREQUENCY`{: .text-blue-000} : The end frequency of the chirp signal in Hz
 
 
 <!-- example text
---
+++ projects/FMCW Radar/FMCW_Measurements.ino
@@ -0,0 +1,96 @@
+#include <SensEdu.h>
+
+// usage example
+// adc1_data is a pointer
+// if it is not, use & symbol before the array
+
+// serial_send_array((const uint8_t *)adc1_data, sizeof(adc1_data)); // second argument in bytes
+
+// for this poor implementation, make sure data is multiple of 32
+
+// send serial data in 32 byte chunks
+void serial_send_array(const uint8_t* data, size_t size) {
+    const size_t chunk_size = 32;
+	for (uint16_t i = 0; i < size/chunk_size; i++) {
+		Serial.write(data + chunk_size * i, chunk_size);
+	}
+}
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                User Settings                               */
+/* -------------------------------------------------------------------------- */
+
+#define CHIRP_DURATION          0.001   // Duration of the chirp (in seconds)
+#define START_FREQUENCY         30300   // Start frequency (in Hz)
+#define END_FREQUENCY           35300  // Stop frequency (in Hz)
+
+/* -------------------------------------------------------------------------- */
+/*                              Global Variables                              */
+/* -------------------------------------------------------------------------- */
+
+static uint32_t lib_error = 0;
+static uint8_t increment_flag = 1; // Run time modification flag
+const float fs = 42 * END_FREQUENCY; // Sampling frequency
+const float samples = fs * CHIRP_DURATION; // Number of samples
+const uint32_t samples_int = (uint32_t)samples;
+static SENSEDU_DAC_BUFFER(lut, samples_int); // Buffer for the chirp signal
+
+/* -------------------------------------------------------------------------- */
+/*                                    Setup                                   */
+/* -------------------------------------------------------------------------- */
+
+void setup() {
+    Serial.begin(115200);
+    while(!Serial);
+
+    // Initialize DAC settings
+    SensEdu_DAC_Settings dac1_settings = {
+        DAC1, fs, (uint16_t*)lut, samples_int,
+        SENSEDU_DAC_MODE_CONTINUOUS_WAVE, 2
+    };
+
+    // Generate the chirp signal
+    generateChirpSignal(lut);
+
+    SensEdu_DAC_Init(&dac1_settings);
+    SensEdu_DAC_Enable(DAC1);
+
+    serial_send_array((const uint8_t *)lut, sizeof(lut), "b");
+
+    // Check for errors
+    lib_error = SensEdu_GetError();
+    while (lib_error != 0) {
+        delay(1000);
+        Serial.print("Error: 0x");
+        Serial.println(lib_error, HEX);
+    }
+
+    Serial.println("Setup is successful.");
+
+    Serial.println("start of the Chirp LUT");
+    for (int i = 0 ; i < samples_int; i++) { // loop for the LUT size
+        Serial.print("value ");
+        Serial.print(i+1);
+        Serial.print(" of the Chirp LUT: ");
+        Serial.println(lut[i]);
+    }
+}
+
+
+/* -------------------------------------------------------------------------- */
+/*                                    Loop                                    */
+/* -------------------------------------------------------------------------- */
+void loop() {
+    // Check for errors
+    lib_error = SensEdu_GetError();
+    while (lib_error != 0) {
+        delay(1000);
+        Serial.print("Error: 0x");
+        Serial.println(lib_error, HEX);
+    }
+
+    /* delay(100);
+    SensEdu_DAC_Enable(DAC1); */
+}

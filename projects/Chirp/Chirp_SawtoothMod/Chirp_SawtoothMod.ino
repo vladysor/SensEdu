300152eb122b50915c7ed5564093cd6684c1c099
PaulThbt <PaulCharles.Thiebaut@infineon.com>
2025-02-28 15:50:32 +0100

    Adjusted user error message in Chirp_SawtoothMod.ino & Chirp_TriangularMod.ino

--- projects/Chirp/Chirp_SawtoothMod/Chirp_SawtoothMod.ino
+++ projects/Chirp/Chirp_SawtoothMod/Chirp_SawtoothMod.ino
@@ -14,10 +14,16 @@
 
 static uint32_t lib_error = 0;
 static uint8_t increment_flag = 1; // Run time modification flag
-const float fs =  100* END_FREQUENCY; // Sampling frequency
+const float fs =  100 * END_FREQUENCY; // Sampling frequency
 const float samples = fs * CHIRP_DURATION; // Number of samples
 const uint32_t samples_int = (uint32_t)samples;
 static SENSEDU_DAC_BUFFER(lut, samples_int); // Buffer for the chirp signal
+
+// Initialize DAC settings
+    SensEdu_DAC_Settings dac1_settings = {
+        DAC_CH2, fs, (uint16_t*)lut, samples_int,
+        SENSEDU_DAC_MODE_CONTINUOUS_WAVE, 1
+    };
 
 /* -------------------------------------------------------------------------- */
 /*                                    Setup                                   */
@@ -27,16 +33,11 @@ void setup() {
     Serial.begin(115200);
     while(!Serial);
 
-  // Check if samples_int exceeds 7688 or fs exceeds 15 MHz
-  if (samples_int > 7688 || fs > 15000000) { // 15 MHz = 15,000,000 Hz
-    #error Error: samples_int exceeds 7688 or fs exceeds 15 MHz!
-  }
-
-    // Initialize DAC settings
-    SensEdu_DAC_Settings dac1_settings = {
-        DAC_CH2, fs, (uint16_t*)lut, 9000,
-        SENSEDU_DAC_MODE_BURST_WAVE, 10
-    };
+    // Check if array size exceeds 7688 or fs exceeds 15 MHz
+    if (samples_int > 7688 || fs > 15000000) {
+        Serial.println("Error: samples_int exceeds 7688 or fs exceeds 15 MHz!");
+        while (true);
+      }
 
     // Generate the chirp signal
     generateSawtoothChirp(lut);
@@ -63,7 +64,6 @@ void setup() {
         Serial.println(lut[i]);
     }
 }
-
 
 /* -------------------------------------------------------------------------- */
 /*                                    Loop                                    */
--- projects/Chirp/Chirp_TriangularMod/Chirp_TriangularMod.ino
+++ projects/Chirp/Chirp_TriangularMod/Chirp_TriangularMod.ino
@@ -1,49 +1,28 @@
 #include <SensEdu.h>
 
-
 /* -------------------------------------------------------------------------- */
 /*                                User Settings                               */
 /* -------------------------------------------------------------------------- */
 
 #define CHIRP_DURATION          0.002    // Duration of the chirp (in seconds)
-#define START_FREQUENCY         30300    // Start frequency (in Hz)
-#define END_FREQUENCY           35300   // Stop frequency (in Hz)
+#define START_FREQUENCY         1000    // Start frequency (in Hz)
+#define END_FREQUENCY           30000   // Stop frequency (in Hz)
 
 /* -------------------------------------------------------------------------- */
 /*                              Global Variables                              */
 /* -------------------------------------------------------------------------- */
 
 static uint32_t lib_error = 0;
-uint8_t error_led = D86;
 static uint8_t increment_flag = 1; // Run time modification flag
-const float fs = 42 * END_FREQUENCY; // Sampling frequency of LUT
+const float fs = 100 * END_FREQUENCY; // Sampling frequency
 const float samples = fs * CHIRP_DURATION; // Number of samples
 const uint32_t samples_int = (uint32_t)samples;
 static SENSEDU_DAC_BUFFER(lut, samples_int*2); // Buffer for the chirp signal
 
 // Initialize DAC settings
 SensEdu_DAC_Settings dac1_settings = {
-    DAC1, fs, (uint16_t*)lut, samples_int*2,
-    SENSEDU_DAC_MODE_CONTINUOUS_WAVE, 0
-};
-
-const uint16_t mic_data_size = 64*32; // must be multiple of 64 for 16bit
-__attribute__((aligned(__SCB_DCACHE_LINE_SIZE))) uint16_t mic_data[mic_data_size]; // cache aligned
-
-ADC_TypeDef* adc = ADC1;
-const uint8_t mic_num = 1;
-uint8_t mic_pins[mic_num] = {A4};
-SensEdu_ADC_Settings adc_settings = {
-    .adc = adc,
-    .pins = mic_pins,
-    .pin_num = mic_num,
-
-    .conv_mode = SENSEDU_ADC_MODE_CONT_TIM_TRIGGERED,
-    .sampling_freq = 250000,
-    
-    .dma_mode = SENSEDU_ADC_DMA_CONNECT,
-    .mem_address = (uint16_t*)mic_data,
-    .mem_size = mic_data_size
+    DAC_CH2, fs, (uint16_t*)lut, samples_int*2,
+    SENSEDU_DAC_MODE_CONTINUOUS_WAVE, 1
 };
 
 /* -------------------------------------------------------------------------- */
@@ -52,16 +31,19 @@ SensEdu_ADC_Settings adc_settings = {
 
 void setup() {
     Serial.begin(115200);
-    
+    while(!Serial);
+
+    // Check if array size exceeds 7688 or fs exceeds 15 MHz
+    if (samples_int*2 > 7688 || fs > 15000000) {
+        Serial.println("Error: samples_int exceeds 7688 or fs exceeds 15 MHz!");
+        while (true);
+      }
+
     // Generate the chirp signal
     generateTriangularChirp(lut);
 
     SensEdu_DAC_Init(&dac1_settings);
-    SensEdu_DAC_Enable(DAC1);
-
-    SensEdu_ADC_ShortA4toA9();
-    SensEdu_ADC_Init(&adc_settings);
-    SensEdu_ADC_Enable(adc);
+    SensEdu_DAC_Enable(DAC_CH2);
 
     // Check for errors
     lib_error = SensEdu_GetError();
@@ -71,42 +53,22 @@ void setup() {
         Serial.println(lib_error, HEX);
     }
 
-    lib_error = SensEdu_GetError();
-    while (lib_error != 0) {
-        handle_error();
-    }
-
     Serial.println("Setup is successful.");
-}
 
+    // Print the chirp signal LUT
+    Serial.println("start of the Chirp LUT");
+    for (int i = 0 ; i < samples_int; i++) { // loop for the LUT size
+        Serial.print("value ");
+        Serial.print(i+1);
+        Serial.print(" of the Chirp LUT: ");
+        Serial.println(lut[i]);
+    }
+}
 
 /* -------------------------------------------------------------------------- */
 /*                                    Loop                                    */
 /* -------------------------------------------------------------------------- */
 void loop() {
-
-    // Measurement is initiated by the signal from computing device
-    static char serial_buf = 0;
-    
-    while (1) {
-        while (Serial.available() == 0); // Wait for a signal
-        serial_buf = Serial.read();
-
-        if (serial_buf == 't') {
-            // expected 't' symbol (trigger)
-            break;
-        }
-    }
-
-    // start dac->adc sequence
-    SensEdu_ADC_Start(adc);
-    
-    // wait for the data and send it
-    while(!SensEdu_DMA_GetADCTransferStatus(ADC1));
-    SensEdu_DMA_ClearADCTransferStatus(ADC1);
-    serial_send_array((const uint8_t *) & mic_data, mic_data_size << 1);
-
-
     // Check for errors
     lib_error = SensEdu_GetError();
     while (lib_error != 0) {
@@ -114,20 +76,4 @@ void loop() {
         Serial.print("Error: 0x");
         Serial.println(lib_error, HEX);
     }
-}
-
-/* -------------------------------------------------------------------------- */
-/*                                  Functions                                 */
-/* -------------------------------------------------------------------------- */
-void handle_error() {
-    // serial is taken by matlab, use LED as indication
-    digitalWrite(error_led, LOW);
-}
-
-// send serial data in 32 byte chunks
-void serial_send_array(const uint8_t* data, size_t size) {
-    const size_t chunk_size = 32;
-	for (uint32_t i = 0; i < size/chunk_size; i++) {
-		Serial.write(data + chunk_size * i, chunk_size);
-	}
 }

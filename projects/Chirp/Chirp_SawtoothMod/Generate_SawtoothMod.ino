6ad061e291aa3eaf1fe45e0be8e6683287771b03
PaulThbt <PaulCharles.Thiebaut@infineon.com>
2025-05-22 17:52:58 +0200

    Code optimization for chirp project to generate larger chirp LUT

--- projects/Chirp/Chirp_SawtoothMod/Generate_SawtoothMod.ino
+++ projects/Chirp/Chirp_SawtoothMod/Generate_SawtoothMod.ino
@@ -5,37 +5,38 @@ const float Pi = 3.14159; // pi
 void generateSawtoothChirp(uint16_t* array) {
 
     float lut_sine[90 * x]; // Quarter-wave LUT for sine values
-    float vChirp[samples_int]; // Chirp signal array
-
+    float vChirp; // Chirp signal array
+    float phase_deg; // Phase angle in degrees
+    float phase_rad; // Phase angle in radians
+    float phase_deg_wrapped; // Wrapped phase angle
+    float sK = (END_FREQUENCY - START_FREQUENCY) / CHIRP_DURATION; // Chirp rate
+    int P=round(CHIRP_DURATION*(START_FREQUENCY+sK*CHIRP_DURATION/2)); // Condition for int number of cycles
+    int END_FREQUENCY_ADJUSTED = 2*P/(CHIRP_DURATION) - START_FREQUENCY; // Adjusted end frequency for int number of cycles
+    sK = (END_FREQUENCY_ADJUSTED - START_FREQUENCY) / CHIRP_DURATION; // Adjusted chirp rate
+    
     // Generate the quarter-wave sine LUT
     for (int i = 0; i < 90 * x; i++) {
-        float phase_deg = (float)i * 90.0 / (90 * x); // Phase angle in degrees
-        float phase_rad = phase_deg * Pi / 180.0; // Phase angle to radians
+        phase_deg = (float)i/x;
+        phase_rad = phase_deg * Pi / 180.0;
         lut_sine[i] = sin(phase_rad-Pi/2); // Store sine value in the LUT
     }
 
     // Generate the chirp signal
-    float sT = CHIRP_DURATION; // Time duration of the chirp signal
-    float sK = (END_FREQUENCY - START_FREQUENCY) / sT; // Chirp rate
-
-    for (int i = 1; i < samples_int + 1; i++) {
-        float phase_rad = 2.0 * Pi * (0.5 * sK * (i - 1) / fs + START_FREQUENCY) * (i - 1) / fs; // Phase angle in radians
-        float phase_deg = phase_rad * 180.0 / Pi; // Phase angle to degrees
-        float phase_deg_wrapped = fmod(phase_deg, 360.0); // Wrap phase angle to 0-360 degrees
+    for (int i = 0; i < samples_int; i++) {
+        phase_rad = 2.0 * Pi * (0.5 * sK * i / fs + START_FREQUENCY) * i / fs; // Phase angle in radians
+        phase_deg = phase_rad * 180.0 / Pi; // Phase angle to degrees
+        phase_deg_wrapped = fmod(phase_deg, 360.0); // Wrap phase angle to 0-360 degrees
 
         if (phase_deg_wrapped <= 90) {
-            vChirp[i - 1] = lut_sine[(int)(phase_deg_wrapped / 90.0 * (90 * x - 1))] * 2047.5 + 2047.5;
+            vChirp = lut_sine[(int)(phase_deg_wrapped)* x+1] * 2048 + 2048;
         } else if (phase_deg_wrapped <= 180) {
-            vChirp[i - 1] = -lut_sine[(int)((180.0 - phase_deg_wrapped) / 90.0 * (90 * x - 1))] * 2047.5 + 2047.5;
+            vChirp = -lut_sine[(int)(180.0 - phase_deg_wrapped) * x+1] * 2048 + 2048;
         } else if (phase_deg_wrapped <= 270) {
-            vChirp[i - 1] = -lut_sine[(int)((phase_deg_wrapped - 180.0) / 90.0 * (90 * x - 1))] * 2047.5 + 2047.5;
+            vChirp = -lut_sine[(int)(phase_deg_wrapped - 180.0) * x+1] * 2048 + 2048;
         } else {
-            vChirp[i - 1] = lut_sine[(int)((360.0 - phase_deg_wrapped) / 90.0 * (90 * x - 1))] * 2047.5 + 2047.5;
+            vChirp = lut_sine[(int)(360.0 - phase_deg_wrapped)* x+1] * 2048 + 2048;
         }
-    }
 
-     // Copy the chirp signal to the DAC buffer
-    for (int i = 0; i < samples_int; i++) {
-        array[i] = (uint16_t)vChirp[i];
+        array[i] = (uint16_t)vChirp;
     }
 }

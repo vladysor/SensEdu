6dbcb22943c4c2f605c4c63d151fe1ceebaf216e
PaulThbt <PaulCharles.Thiebaut@infineon.com>
2025-05-23 16:47:24 +0200

    Chirp triangular modulation supports large LUT & fixed chirp duration logic

--- projects/Chirp/Chirp_TriangularMod/Chirp_TriangularMod.ino
+++ projects/Chirp/Chirp_TriangularMod/Chirp_TriangularMod.ino
@@ -4,9 +4,9 @@
 /*                                User Settings                               */
 /* -------------------------------------------------------------------------- */
 
-#define CHIRP_DURATION          0.002    // Duration of the chirp (in seconds)
-#define START_FREQUENCY         1000    // Start frequency (in Hz)
-#define END_FREQUENCY           30000   // Stop frequency (in Hz)
+#define CHIRP_DURATION          0.05    // Duration of the chirp (in seconds)
+#define START_FREQUENCY         30500    // Start frequency (in Hz)
+#define END_FREQUENCY           35500   // Stop frequency (in Hz)
 
 /* -------------------------------------------------------------------------- */
 /*                              Global Variables                              */
@@ -14,14 +14,14 @@
 
 static uint32_t lib_error = 0;
 static uint8_t increment_flag = 1; // Run time modification flag
-const float fs = 100 * END_FREQUENCY; // Sampling frequency
+const float fs = 50 * END_FREQUENCY; // Sampling frequency
 const float samples = fs * CHIRP_DURATION; // Number of samples
 const uint32_t samples_int = (uint32_t)samples;
-static SENSEDU_DAC_BUFFER(lut, samples_int*2); // Buffer for the chirp signal
+static SENSEDU_DAC_BUFFER(lut, samples_int); // Buffer for the chirp signal
 
 // Initialize DAC settings
 SensEdu_DAC_Settings dac1_settings = {
-    DAC_CH2, fs, (uint16_t*)lut, samples_int*2,
+    DAC_CH2, fs, (uint16_t*)lut, samples_int,
     SENSEDU_DAC_MODE_CONTINUOUS_WAVE, 1
 };
 
@@ -32,12 +32,6 @@ SensEdu_DAC_Settings dac1_settings = {
 void setup() {
     Serial.begin(115200);
     while(!Serial);
-
-    // Check if array size exceeds 7688 or fs exceeds 15 MHz
-    if (samples_int*2 > 7688 || fs > 15000000) {
-        Serial.println("Error: samples_int*2 exceeds 7688 or fs exceeds 15 MHz!");
-        while (true);
-      }
 
     // Generate the chirp signal
     generateTriangularChirp(lut);

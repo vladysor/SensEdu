2590b5f0ca3795d6532699ce6cde51f7a93f6921
Vladyslav Sorokin <Vladyslav.Sorokin@infineon.com>
2025-05-28 18:54:10 +0200

    additional dataset

--- projects/EMG-BioInputs/matlab/data_analysis.m
+++ projects/EMG-BioInputs/matlab/data_analysis.m
@@ -4,13 +4,14 @@ clear;
 close all;
 
 %% Settings
-filename = 'muscles_set_relax_swings_relax_long_relax_5short_relax_long.mat';
+filename = 'muscles_set_relax_BpressOften_Run_relax_long.mat';
 
 %% logic
 load(filename);
 
-data1 = construct_dataset(raw_data);
-data2 = construct_dataset(processed_data);
+channel = 3;
+data1 = construct_dataset(raw_data, channel);
+data2 = construct_dataset(processed_data, channel);
 
 subplot(1,2,1);
 plot(data1);
@@ -20,18 +21,18 @@ plot(data2);
 %% processing
 data3 = data2 - (mean(data2));
 data3 = abs(data3);
-data4 = envelope(data3, 2000, "rms");
+data4 = envelope(data3, 1000, "rms");
 figure;
 plot(data3);
 hold on;
 plot(data4);
 
-function data = construct_dataset(dataset)
+function data = construct_dataset(dataset, channel)
     data = zeros(1,size(dataset,2)*size(dataset,3));
     j = 1;
     it = 1;
     for i = 1:length(data)
-        data(i) = dataset(1,j,it);
+        data(i) = dataset(channel,j,it);
         if mod(i,size(dataset,2)) == 0
             j = 1;
             it = it + 1;

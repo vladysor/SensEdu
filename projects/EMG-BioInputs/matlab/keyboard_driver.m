3a656d3658ca01e32d9d9942732f9b443c84540e
Vladyslav Sorokin <Vladyslav.Sorokin@infineon.com>
2025-06-18 19:17:51 +0200

    keys pressing and square wave plotting

--- projects/EMG-BioInputs/matlab/keyboard_driver.m
+++ projects/EMG-BioInputs/matlab/keyboard_driver.m
@@ -22,8 +22,12 @@ PLOT_ON = true;
 % Saving
 SAVE_ON = false;
 FOLDERNAME = "measurements";
-SETNAME = "MuscleSet4";
+SETNAME = "MuscleSet";
 IS_OVERWRITE = true;
+
+% Thresholds
+PRESS_RANGE_PERCENTAGE = 0.2;
+RELEASE_RANGE_PERCENTAGE = 0.1;
 
 %% Keyboard
 import java.awt.Robot;
@@ -31,8 +35,8 @@ import java.awt.event.*;
 robot = java.awt.Robot();
 robot.delay(2000);
 
-keys = [InputEvent.BUTTON1_DOWN_MASK, InputEvent.BUTTON1_DOWN_MASK, InputEvent.BUTTON1_DOWN_MASK, KeyEvent.VK_ALT];
-keys_status = [false, false, false, false];
+keys = [InputEvent.BUTTON1_DOWN_MASK, InputEvent.BUTTON1_DOWN_MASK, InputEvent.BUTTON1_DOWN_MASK, InputEvent.BUTTON1_DOWN_MASK];
+keys_state = zeros(1, numel(keys));
 
 %% Arduino Connection
 arduino = serialport(ARDUINO_PORT, ARDUINO_BAUDRATE);
@@ -56,6 +60,12 @@ fs = double(typecast_uint8(read(arduino, 2, 'uint8')));
 data_length = double(mem_size/channel_n);
 meas_duration_ms = 1000*(data_length/fs);
 
+if numel(keys) ~= channel_n
+    error_msg = ['Configured keys count does not match channel count.\n',
+                 'Correct "keys" and "keys_state" vectors to match arduino configuration.'];
+    error(error_msg, numel(keys));
+end
+
 %% Initialization
 % Buffer Init
 meas_n = BUFFER_DURATION_MS/meas_duration_ms; % number of measurements in one buffer
@@ -70,15 +80,9 @@ cut_filtered_samples = filter_delay; % cut some additional samples for better si
                                      % be careful with data trimming: 
                                      % delay appears on signal end
                                      % additional cut on signal start
-% Processed Plotting
+% Filtered X axis
 processed_x = 1:size(buffer,2);
 processed_x = processed_x((cut_filtered_samples+1):(end-filter_delay));
-plotting_offsets = zeros(1, channel_n);
-
-% Saving Sequence
-if SAVE_ON == true
-    prepare_save_folders(FOLDERNAME, SETNAME, IS_OVERWRITE);
-end
 
 % Maximum Values
 threshold_analyzed_x = round(1/4 * size(processed_x, 2)):round(3/4 * size(processed_x, 2));
@@ -93,6 +97,32 @@ last_1min_max_values = zeros(channel_n, 1*60*(1000/meas_duration_ms));
 % Thresholds
 press_thresholds = zeros(channel_n, 1);
 release_thresholds = zeros(channel_n, 1);
+
+% Keyboard Emulation
+keyboard_analyzed_samples = samples_per_meas_after_cut;
+
+% Saving Sequence
+if SAVE_ON == true
+    prepare_save_folders(FOLDERNAME, SETNAME, IS_OVERWRITE);
+end
+
+% Plotting
+plotting_offsets = zeros(1, channel_n); % centering for raw samples
+keys_plotting_x = processed_x(end); % x axis for key square wave;
+i = 1; % chunk counter
+while(true)
+    start_x = processed_x(end) - keyboard_analyzed_samples*i + 1;
+    end_x = start_x - 1;
+    if start_x < processed_x(1)
+        break;
+    end
+    keys_plotting_x = [keys_plotting_x, start_x, end_x];
+    i = i + 1;
+end
+clear i;
+keys_plotting_x = wrev(keys_plotting_x);
+keys_plotting_x = keys_plotting_x(2:end);
+keys_plotting_y = zeros(channel_n, numel(keys_plotting_x));
 
 %% Main Loop
 counter = 1;
@@ -128,7 +158,6 @@ while(true)
     end
 
     % Correct MAX values
-    % you can try prctile(data, 95) as alternative
     % analyze only middle of the envelope for increased precision
     loop_max = max(enveloped_buffer(:,threshold_analyzed_x), [], 2);
     all_history_max = max([all_history_max, loop_max], [], 2);
@@ -139,9 +168,14 @@ while(true)
     current_max = adjust_current_max(all_history_max, last_1min_max, last_1sec_max);
     
     % Thresholds
-    press_thresholds = 0.2.*current_max;
-    release_thresholds = 0.1.*current_max;
-
+    press_thresholds = PRESS_RANGE_PERCENTAGE.*current_max;
+    release_thresholds = RELEASE_RANGE_PERCENTAGE.*current_max;
+    
+    % Keyboard Emulation
+    keys_actions = analyze_keys_state(enveloped_buffer(:,(end-keyboard_analyzed_samples+1):end), press_thresholds, release_thresholds);
+    keys_state = release_keys(robot, keys, keys_state, keys_actions(1,:));
+    keys_state = press_keys(robot, keys, keys_state, keys_actions(2,:));
+    
     % Save Data
     if SAVE_ON == true
         save_data(FOLDERNAME, SETNAME, save_loop, processed_x, buffer, plotting_offsets, rectified_buffer, enveloped_buffer, ...
@@ -155,7 +189,8 @@ while(true)
         plot_data(processed_x, buffer, plotting_offsets, rectified_buffer, enveloped_buffer);
         %plot_max(size(buffer, 2), current_max, all_history_max, last_1min_max, last_1sec_max);
         plot_thresholds(size(buffer, 2), current_max, press_thresholds, release_thresholds);
-        plot_keys(); % here put square wave that represents pressed and released keys
+        keys_plotting_y = [keys_plotting_y(:, 3:end), keys_state', keys_state'];
+        plot_keys(keys_plotting_x, keys_plotting_y);
     end
     toc;
 end
@@ -214,30 +249,6 @@ function check_firmware_error(data, channel_n)
     if error_flag == 1
         error(['Internal Firmware Error. Code: %s. Reset the board before proceeding.\n' ...
             'https://shiegechan.github.io/SensEdu/Library/ADC/#errors\nLook for corresponding error code in the wiki.'], string(dec2hex(data(1,3))));
-    end
-end
-
-function new_key_status = press_key(data, PRESS_THRESHOLD, key, key_status, robot)
-    %figure(5);
-    %plot(data);
-
-
-    new_key_status = key_status;
-    if (any(data > PRESS_THRESHOLD))
-        if key_status == false
-            % press only if isn't already pressed (fix this for better pushing)
-            robot.mousePress(key);
-            robot.delay(50);
-            robot.mouseRelease(key);
-            %new_key_status = true;
-            fprintf(datestr(datetime('now'), 'HH:MM:SS') + " - Button Pressed.\n");
-        end
-    else
-        if key_status == true
-            robot.mouseRelease(key);
-            new_key_status = false;
-            fprintf(datestr(datetime('now'), 'HH:MM:SS') + " - Button Released.\n");
-        end
     end
 end
 
@@ -304,8 +315,10 @@ end
 function current_max = adjust_current_max(all_history_max, last_1min_max, last_1sec_max)
     current_max = 0.0.*all_history_max + 0.3.*last_1min_max + 0.7*last_1sec_max;
     for i = 1:size(all_history_max, 1)
-        if current_max(i) < (0.25*all_history_max(i))
-            current_max(i) = 0.25*all_history_max(i);
+        %limit = 0.25*all_history_max(i);
+        limit = 50; % hard coded value here is less error prone
+        if current_max(i) < limit
+            current_max(i) = limit;
         end
     end
 end
@@ -329,6 +342,37 @@ function plot_thresholds(max_index, current_max, press_thresholds, release_thres
         plot([1,max_index], [current_max(i), current_max(i)], 'color', '#000000', 'linewidth', 2.5);
         plot([1,max_index], [press_thresholds(i), press_thresholds(i)], 'color', '#6B8E23', 'linewidth', 2.5);
         plot([1,max_index], [release_thresholds(i), release_thresholds(i)], 'color', '#FF8C00', 'linewidth', 2.5);
+        hold off;
+    end
+end
+
+function keys_actions = analyze_keys_state(data, press_thresholds, release_thresholds)
+    released_states = any(data < release_thresholds, 2)';
+    pressed_states = any(data > press_thresholds, 2)';
+    keys_actions = [released_states; pressed_states];
+end
+
+function keys_state = release_keys(robot, keys, keys_state, keys_release)
+    to_release = find(keys_state == 1 & keys_release == 1);
+    for i = to_release
+        robot.mouseRelease(keys(i));
+        keys_state(i) = 0;
+    end
+end
+
+function keys_state = press_keys(robot, keys, keys_state, keys_press)
+    to_press = find(keys_state == 0 & keys_press == 1);
+    for i = to_press
+        robot.mousePress(keys(i));
+        keys_state(i) = 1;
+    end
+end
+
+function plot_keys(keys_plotting_x, keys_plotting_y)
+    for i = 3 % 1:size(keys_plotting_y, 1)
+        subplot(1,size(keys_plotting_y, 1),i);
+        hold on;
+        plot(keys_plotting_x, 200.*keys_plotting_y(i,:), 'color', '#29505d', 'linewidth', 2.5);
         hold off;
     end
 end

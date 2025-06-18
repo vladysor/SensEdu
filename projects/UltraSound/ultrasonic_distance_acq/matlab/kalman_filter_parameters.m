763fe98fdaa7ab2387a33a4b951f08f9ac90add0
Azra <Azra.Talic@infineon.com>
2025-06-18 13:51:55 +0200

    matlab scripts restructure

--- projects/UltraSound/ultrasonic_distance_acq/matlab/kalman_filter_parameters.m
+++
@@ -1,4 +0,0 @@
-% kalman filter params
-sigma_p = 0.;
-sigma_q = 0.2; 
-sigma_r = 0.05^2;
--- projects/UltraSound/ultrasonic_distance_acq_xl/matlab/post_processing.m
+++
@@ -1,8 +0,0 @@
-clear;
-load("dist_kf.mat")
-MIC_NUM = 6;
-figure
-for i = 1:MIC_NUM
-    plot(dist_matrix(i, :), 'LineWidth', 2); hold on;
-end
-hold off;

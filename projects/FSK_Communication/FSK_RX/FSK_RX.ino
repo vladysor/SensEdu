cf2297b22033e00c3342e899140b4627af1bc3b9
Ferni41359 <Fernando.GomezGarcia@infineon.com>
2026-02-09 11:41:27 +0100

    feat: Implement wireless FSK communication (#85)
    - Change waves to 32kHz and 34kHz
    - LUTs have now different sizes
    - Add not multiple frequencies 31kHz and 35kHz

--- projects/FSK_Communication/FSK_RX/FSK_RX.ino
+++ projects/FSK_Communication/FSK_RX/FSK_RX.ino
@@ -1,5 +1,4 @@
 #include "SensEdu.h"
-
 /* errors */
 static uint32_t lib_error = 0; // Internal library error container
 uint8_t error_led = D86; 
@@ -8,16 +7,32 @@ uint8_t error_led = D86;
 /*                                  Settings                                  */
 /* -------------------------------------------------------------------------- */
 
-const uint16_t THRESHOLD = 4000;  // If the signal goes bellow, synchro starts
-float fs = 32800 * 32;  // Your sample rate
-float f0 = fs / SAMPLES_PER_BIT;       // YOUR low frequency
-float f1 = fs * 2 / SAMPLES_PER_BIT;       // YOUR high frequency
+const uint16_t THRESHOLD = 500;  // If the signal goes higher, synchro starts
 const int SYNC_PIN = 2; // Syncronization signal from PIN 2 digital
-const uint16_t SAMPLES_PER_BIT = 64;
+const int WINDOW_SIZE = 50;  // Size of sliding window 
+
+const uint16_t SAMPLES_PER_BIT = 272;
 const uint16_t BIT_PER_BYTE = 8;
-const uint16_t MAX_MESSAGE_LENGTH = 64;
-const uint16_t mic_data_size = MAX_MESSAGE_LENGTH * BIT_PER_BYTE * SAMPLES_PER_BIT;  //match MAX_LUT_SIZE
-SENSEDU_ADC_BUFFER(mic_data, mic_data_size);
+const uint16_t MAX_MESSAGE_LENGTH = 30;
+const uint16_t MIC_DATA_SIZE = MAX_MESSAGE_LENGTH * BIT_PER_BYTE * SAMPLES_PER_BIT;  //match MAX_LUT_SIZE
+SENSEDU_ADC_BUFFER(MIC_DATA, MIC_DATA_SIZE);
+
+int current_sample_ptr = 0; 
+int signal_start_pos = 0;
+bool signal_found = false;
+
+/* -------------------------------------------------------------------------- */
+/*                                Frequencies                                 */
+/* -------------------------------------------------------------------------- */
+
+float fs = 33000 * 32;  // Your sample rate, double than transmitter for better resolution
+const uint16_t NUMBER_CYCLES = 8; // Number of periods per bit
+
+const uint16_t BIT0_SAMPLE_PER_CYCLE = 34;
+float f0 = fs / BIT0_SAMPLE_PER_CYCLE; // Bit 0 Frequency ~31kHz
+
+const uint16_t BIT1_SAMPLE_PER_CYCLE = 30;
+float f1 = fs / BIT1_SAMPLE_PER_CYCLE; // Bit 1 Frequency ~35kHz
 
 /* ----------------------------------- ADC ---------------------------------- */
 ADC_TypeDef* adc = ADC1;
@@ -28,10 +43,10 @@ SensEdu_ADC_Settings adc_settings = {
     .pins = mic_pins,
     .pin_num = mic_num,
     .sr_mode = SENSEDU_ADC_SR_MODE_FIXED,
-    .sampling_rate_hz = 32800 * 32,
+    .sampling_rate_hz = fs,
     .adc_mode = SENSEDU_ADC_MODE_DMA_NORMAL,
-    .mem_address = (uint16_t*) mic_data,
-    .mem_size = mic_data_size
+    .mem_address = (uint16_t*) MIC_DATA,
+    .mem_size = MIC_DATA_SIZE
 };
 
 /* -------------------------------------------------------------------------- */
@@ -41,7 +56,6 @@ SensEdu_ADC_Settings adc_settings = {
 void setup() {
     Serial.begin(115200);
     Serial.println("Started Initialization...");
-
     //Led in red if there is any problem
     pinMode(error_led, OUTPUT);
     digitalWrite(error_led, HIGH);
@@ -52,7 +66,6 @@ void setup() {
     SensEdu_ADC_Init(&adc_settings);
     SensEdu_ADC_Enable(adc);
 }
-
 /* -------------------------------------------------------------------------- */
 /*                                    Loop                                    */
 /* -------------------------------------------------------------------------- */
@@ -65,46 +78,47 @@ void loop() {
     SensEdu_ADC_ClearDmaTransferComplete(adc);
     while (digitalRead(SYNC_PIN) == HIGH) {};  // Wait for the signal to stop
 
-    // Dynamic offset: find when the signal starts
-    int dynamic_offset = 0;
+    // Calculate the power of a sliding window with a shift of 5 samples
+   for (size_t i = 0; i < MIC_DATA_SIZE - SAMPLES_PER_BIT; i += 5) { // 
+        float p0 = goertzel(&MIC_DATA[i], WINDOW_SIZE, f0, fs);
+        float p1 = goertzel(&MIC_DATA[i], WINDOW_SIZE, f1, fs);
     
-    for (size_t i = 0; i < mic_data_size; i++) {
-        if (mic_data[i] < THRESHOLD) {
-            dynamic_offset = i;
-            break;
+        if ((p0 + p1) > THRESHOLD) {
+            signal_start_pos = i;
+            current_sample_ptr = i + SAMPLES_PER_BIT; // Skip preamble
+            signal_found = true;
+            break; // You found the signal!
         }
     }
 
-    float fs = 32800 * 32;  // Your sample rate
-    float f0 = fs / SAMPLES_PER_BIT;       // YOUR low frequency
-    float f1 = fs * 2 / SAMPLES_PER_BIT;       // YOUR high frequency
-
     for (size_t byte_idx = 0; byte_idx < MAX_MESSAGE_LENGTH; byte_idx++) {
         uint8_t received_byte = 0;
-        float total_power = 0; //Acumulate power
+        float total_power = 0;
 
-        for (size_t bit_pos = 7; bit_pos >= 0; bit_pos--) {
+        for (int32_t bit_pos = 7; bit_pos >= 0; bit_pos--) { 
             // Calculate position in buffer
-            int bit_index = byte_idx * BIT_PER_BYTE + (7 - bit_pos);
-            uint16_t* bit_samples = &mic_data[dynamic_offset + bit_index * SAMPLES_PER_BIT];  //maybe add an offset to skip some data int offset = 10 (?)
+            uint16_t* bit_samples = &MIC_DATA[current_sample_ptr + 20]; // Give 20 samples to the microphones to reach the frequency
 
             // Run Goertzel
-            float power_f0 = goertzel(bit_samples, SAMPLES_PER_BIT, f0, fs);
-            float power_f1 = goertzel(bit_samples, SAMPLES_PER_BIT, f1, fs);
-
+            float power_f0 = goertzel(bit_samples, 220, f0, fs); 
+            float power_f1 = goertzel(bit_samples, 220, f1, fs);
             total_power += power_f0 + power_f1; //Add powers
 
             // Decode bit
             bool bit = (power_f1 > power_f0);
+            
             if (bit) {
                 received_byte |= (1 << bit_pos);
+                current_sample_ptr += (BIT1_SAMPLE_PER_CYCLE * NUMBER_CYCLES); // The size of bit '1' is 240 samples
+            } else {
+                current_sample_ptr += (BIT0_SAMPLE_PER_CYCLE * NUMBER_CYCLES); // The size of bit '0' is 272 samples
             }
         }
+
         // If average power is too low, then it is silence
-        if (total_power / 8 < 1000000) {  // Adjust depending on your signals 
+        if (total_power / 8 < 10000) {  // Adjust depending on your signals 
             break;
         }
-
         // Send decoded character
         Serial.write(received_byte);
     }
@@ -123,21 +137,20 @@ void check_errors() {
 // Goertzel function: analyses one selectable frequency component from a discrete signal
 float goertzel(uint16_t* samples, int N, float targetFreq, float sampleRate) {
     // Implementation of the filter
-    float k = round((N * targetFreq) / sampleRate);  
+    float k = (N * targetFreq) / sampleRate;  
     float omega = (2.0 * PI * k) / N;
     float coeff = 2.0 * cos(omega);
-
+    
     //DC offset of our samples
     float sum = 0;
-    for(size_t i = 0; i < N; i++) {
+    for (size_t i = 0; i < N; i++) {
         sum += samples[i];
     }
     float dc_center = sum / N;
-
     float q0 = 0, q1 = 0, q2 = 0;
     for (size_t i = 0; i < N; i++) {
         // Remove DC offset
-        float sample = (float) (samples[i] - dc_center); 
+        float sample = (float) (samples[i] - dc_center) / 1000; 
         q0 = coeff * q1 - q2 + sample;
         q2 = q1;
         q1 = q0;
@@ -145,3 +158,4 @@ float goertzel(uint16_t* samples, int N, float targetFreq, float sampleRate) {
     float result =  q1 * q1 + q2 * q2 - q1 * q2 * coeff;
     return result;
 }
+
--- projects/FSK_Communication/FSK_TX/Extra/matlab/modulated_wave.m
+++ projects/FSK_Communication/FSK_TX/Extra/matlab/modulated_wave.m
@@ -3,22 +3,17 @@ clear;
 close all;
 
 % Parameters
-sampleRate =  32000;         % Fixed size for both
-f0 = 30000;                  % Target frequency for bit 0
-f1 = 40000;                  % Target frequency for bit 1
-LUT_SIZE_0 = round(64*sampleRate/f0);  % LUT size for bit 0
-LUT_SIZE_1 = round(64*sampleRate/f1);  % LUT size for bit 1
-
-
-% % Calculate how many cycles fit in 64 samples
-% cycles_f0 = (f0 * LUT_SIZE) / sampleRate_0;  % 6.4 cycles
-% cycles_f1 = (f1 * LUT_SIZE) / sampleRate_1;  % 12.8 cycles
+sampleRate =  33000;         % Fixed size for both
+f0 = 31000;                  % Target frequency for bit 0
+f1 = 35000;                  % Target frequency for bit 1
+LUT_SIZE_0 = round(16*sampleRate/f0);  % LUT size for bit 0
+LUT_SIZE_1 = round(16*sampleRate/f1);  % LUT size for bit 1
 
 % Generate LUTs with multiple cycles
-t_0 = linspace(0, 1/30000, LUT_SIZE_0);
+t_0 = linspace(0, 1/f0, LUT_SIZE_0);
 LUT_f0 = sin(f0*2*pi*t_0-pi/2);
 
-t_1 = linspace(0, 1/40000, LUT_SIZE_1);
+t_1 = linspace(0, 1/f1, LUT_SIZE_1);
 LUT_f1 = sin(f1*2*pi*t_1-pi/2);
 
 % Scale to 0-4095
@@ -31,7 +26,7 @@ hex_char_array_1 = "0x" + string(dec2hex(LUT_f1_scaled));
 
 %% Print in hexadecimal %%
 fprintf(' First LUT: \n');
-for i = 1:68
+for i = 1:17
     fprintf('%s, ', hex_char_array_0(i));
     if (mod(i, 8) == 0)
         fprintf('\n');
@@ -39,7 +34,7 @@ for i = 1:68
 end
 
 fprintf('\nSecond LUT: ');
-for i = 1:51
+for i = 1:15
     fprintf('%s, ', hex_char_array_1(i));
     if (mod(i, 8) == 0)
         fprintf('\n');
